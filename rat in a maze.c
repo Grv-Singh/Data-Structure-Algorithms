e53fa64d2ed090f6399ed12e8a237227bfe3a3a1
Jorge Rubio De Limon <jorgeantifa@gmail.com>
2019-10-12 18:21:45 +0200

    Rename every 'aray' to a'rray'
    Hi Gaurav Singh!
    I've been changing yours 'aray' for 'array'

--- rat in a maze.c
+++ rat in a maze.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<conio.h>
 
-int aray[4][4]={1,0,0,0,1,1,0,1,0,1,0,0,1,1,1,1},arayR[4][4]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},pos[6];
+int array[4][4]={1,0,0,0,1,1,0,1,0,1,0,0,1,1,1,1},arrayR[4][4]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},pos[6];
 int i=0,j=0;
 
 void main(){
@@ -13,81 +13,81 @@ hunt(){
         if(order=1){
             if(pos[2]==1){
                 if(pos[3]==0){
-                    moveR(i,j,aray,arayR);
+                    moveR(i,j,array,arrayR);
                 }
                 if(pos[3]==1){
-                    moveD(i,j,aray,arayR);
+                    moveD(i,j,array,arrayR);
                 }
             }
             if(pos[2]==0 && pos[3]==1){
-                moveD(i,j,aray,arayR);
+                moveD(i,j,array,arrayR);
             }
         }
             if(order=0){
                 if(pos[4]==1 && pos[5]==0){
-                    moveU(i,j,aray,arayR);
+                    moveU(i,j,array,arrayR);
                 }
                 if(pos[4]==0 && pos[5]==1){
-                    moveL(i,j,aray,arayR);
+                    moveL(i,j,array,arrayR);
                 }
             }
 }
 
-moveD(int i, int j, int aray[][], int arayR[][]){
+moveD(int i, int j, int array[][], int arrayR[][]){
 
-if(aray[i+1][j]==0){
-    aray[i][j]=0;
+if(array[i+1][j]==0){
+    array[i][j]=0;
         i++;
-    aray[i][j]=2;
-    arayR[i][j]=1;
-    pos[6]={i,j,aray[i][j+1],aray[i+1][j],aray[i][j-1],aray[i-1][j]};
+    array[i][j]=2;
+    arrayR[i][j]=1;
+    pos[6]={i,j,array[i][j+1],array[i+1][j],array[i][j-1],array[i-1][j]};
 }else{
     return 1;
 }
 
 }
 
-moveR(int i, int j, int aray[][], int arayR[][]){
+moveR(int i, int j, int array[][], int arrayR[][]){
 
-if(aray[i][j+1]==0){
-    aray[i][j]=0;
+if(array[i][j+1]==0){
+    array[i][j]=0;
         j++;
-    aray[i][j]=2;
-    arayR[i][j]=1;
+    array[i][j]=2;
+    arrayR[i][j]=1;
     //moveR(i,j,aray,arayR);
-    pos[6]={i,j,aray[i][j+1],aray[i+1][j],aray[i][j-1],aray[i-1][j]};
+    pos[6]={i,j,array[i][j+1],array[i+1][j],array[i][j-1],array[i-1][j]};
 }
-if(aray[i][j+1]==1){
+if(array[i][j+1]==1){
     return 1;
 }
-if(aray[i][j+1]==2){
+if(array[i][j+1]==2){
     return 2;
 }
 
 }
 
-moveU(int i, int j, int aray[][], int arayR[][]){
+moveU(int i, int j, int array[][], int arrayR[][]){
 
-if(aray[i-1][j]==0){
-    aray[i][j]=0;
+if(array[i-1][j]==0){
+    array[i][j]=0;
         i--;
-    aray[i][j]=2;
-    arayR[i][j]=1;
-    moveU(i,j,aray,arayR);
+    array[i][j]=2;
+    arrayR[i][j]=1;
+    moveU(i,j,array,arrayR);
 }else{
     return 1;
 }
 
 }
 
-moveL(int i, int j, int aray[][], int arayR[][]){
+moveL(int i, int j, int array[][], int arrayR[][]){
 
-if(aray[i][j-1]==0){
-    aray[i][j]=0;
+if(array[i][j-1]==0){
+    array[i][j]=0;
         j--;
-    aray[i][j]=2;
-    arayR[i][j]=1;
-    moveL(i,j,aray,arayR);
+    array[i][j]=2;
+    arrayR[i][j]=1;
+    moveL(i,j,array,arrayR);
 }else{
     return 1;
 }
